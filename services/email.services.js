947a03cace2e270a9cec825bd12ebf0412dd26b9
ayomikun286 <edegbaiayomikun@gmail.com>
2026-08-13 01:20:02 +0100

    fix nodemuller

--- services/email.services.js
+++ services/email.services.js
@@ -1,17 +1,19 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_TO,
         pass: process.env.EMAIL_PASS,
     },
 });
 
-export const sendEmail = async ({subject, html }) => {
+export const sendEmail = async ({ subject, html }) => {
     await transporter.sendMail({
         from: process.env.EMAIL_TO,
-        to:process.env.EMAIL_TO,
+        to: process.env.EMAIL_TO,
         subject,
         html,
     });
